ffae3198860164a6a08cf9887d9278d16705309c
kvcsmiki <kovacs.miklos.szabolcs@gmail.com>
2026-04-01 12:25:53 +0200

    Comment access-control-allow-origin setting

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ const supabase = createClient(
 const JWT_SECRET = process.env.JWT_SECRET
 
 export default async function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL)
+    //res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL)
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS')
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization')
 
